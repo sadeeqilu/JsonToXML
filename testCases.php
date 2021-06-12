6f768e02f08b6384b09434c967c2a4b5767cd3e4
sadeeqilu <ilusadiq@gmail.com>
2021-06-12 10:49:32 +0100

    test cases in progress

--- testCases.php
+++ testCases.php
@@ -3,31 +3,45 @@
 
 function test($data)
 {
-    $defaults = array(
-        CURLOPT_URL => 'http://localhost/JsonToXML/index.php',
-        CURLOPT_POST => true,
-        CURLOPT_POSTFIELDS => http_build_query($data),
-    );
+    // $defaults = array(
+    //     CURLOPT_URL => 'http://localhost/JsonToXML/index.php',
+    //     CURLOPT_POST => true,
+    //     CURLOPT_POSTFIELDS => http_build_query($data),
+    // );
+
         $ch = curl_init();
-        curl_setopt_array($ch, ($options + $defaults));
+        $url = 'http://localhost/JsonToXML/index.php';
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         $result = curl_exec($ch);
+        if($e = curl_error($ch)){
+            echo $e;
+        }else{
+            $decode_result = json_decode($result,true);
+        }
         curl_close($ch);
-        return json_decode($result,true);
+        return $decode_result;
 }
 
 echo "Tests on JsonToXML API.";
 
-$totalTests = 0;
 $passed = 0;
-$failed = 0;
 
-$data = [];
+$data1 = [];
+$data2 = ["from_msisdn"=>123];
+$data3 = ["from_msisdn" => 123,"to_msisdn" => 456];
 try{
-    $test1 = test($data);
+    $test1 = test($data1);
+    $test2 = test($data2);
+    $test3 = test($data3);
     if($test1 == ['from_msisdn field is required'])
-    {
-        $passed += 1;
-    }
+        $passed++;
+    if($test2 == ["to_msisdn field is required"])
+        $passed++;
+    if($test3 == ["message field is required"])
+        $passed++;
 
     echo "Passed test cases = ".$passed;
 }catch(\Exception $exception){
