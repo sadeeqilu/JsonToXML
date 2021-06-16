df83dfcbfc2a3e76cd43e7779786c08f85ed5c1d
sadeeqilu <ilusadiq@gmail.com>
2021-06-16 08:19:52 +0100

    fixed index file

--- index.php
+++ index.php
@@ -7,10 +7,6 @@ header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Max-Age: 1000');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization');
 
-assert_options(ASSERT_ACTIVE, 1);
-assert_options(ASSERT_WARNING, 0);
-assert_options(ASSERT_QUIET_EVAL, 1);
-
 //recursive function for changing json data to xml
 function arrayToXml($array, $parentkey="", $xml = false){
 
@@ -32,66 +28,78 @@ function arrayToXml($array, $parentkey="", $xml = false){
  }
 
  // get post data
-$data = $_POST;
+$data = $json_decode(file_get_contents('php://input'));
 
 // check if all required fields are available
-// display error message for any first missing required field
-assert('!isset($data->from_msisdn)','from_msisdn field is required');
-assert('!isset($data->to_msisdn)','to_msisdn field is required');
-assert('!isset($data->message)','message field is required');
-try{
+if($data->from_msisdn && $data->to_msisdn && $data->message){
+	// display error message for any first missing required field
 
-	// validate input data types
-	assert('is_int($data->from_msisdn);', 'Invalid integer input');
-	assert('is_string($data->message);','Invalid string input');
-	assert('is_int($data->to_msisdn);','Invalid integer input');
+	try{
 
-	// if data has more than 4 inputs, that means extra fields are in the request as well
-	if(count($data) > 4){
-		// check if field_map variable is available
-		assert('isset($data->field_map)',"field_map does not exist");
+		// validate input data types
+		if(!is_int($data->from_msisdn))
+			throwError('Invalid integer input');
+		if(is_string($data->message))
+			throwError('Invalid string input');
+		if(is_int($data->to_msisdn))
+			throwError('Invalid integer input');
 
-		// loop through all data to get extra fields
-		foreach($data as $key){
-			// if key is one the required fields or the field_map object, move to the next key
-			if($key == "from_msisdn" || $key == "to_msisdn" || $key == "message" || $key == "field_map")
-				continue;
-			else {
-				// check if key is in field_map object
-				if(array_key_exists($key,$data['field_map'])){
-					// get the type of the field
-					$type = $data['field_map'][$key];
-					// validate the key type
-					switch($type){
-						case 'boolean':
-							$check = assert('is_bool($data[$key])','Invalid boolean input.');
-						break;
-						case 'integer':
-							$check = assert('is_int($data[$key])','Invalid integer input');
-						break;
-						case 'string':
-							$check = assert('is_string($data[$key])','Invalid string input');
-						break;
-						case 'float':
-							$check = assert('is_float($data[$key])','Invalid float input.');
-						break;
-						default:
-						throwError($type . " is not a valid type."); // type not found
+		// if data has more than 4 inputs, that means extra fields are in the request as well
+		if(count($data) > 4){
+			// check if field_map variable is available
+			if(!isset($data->field_map))
+				throwError("field_map does not exist");
+
+			// loop through all data to get extra fields
+			foreach($data as $key){
+				// if key is one the required fields or the field_map object, move to the next key
+				if($key == "from_msisdn" || $key == "to_msisdn" || $key == "message" || $key == "field_map")
+					continue;
+				else {
+					// check if key is in field_map object
+					if(array_key_exists($key,$data['field_map'])){
+						// get the type of the field
+						$type = $data['field_map'][$key];
+						// validate the key type
+						switch($type){
+							case 'boolean':
+								$check = is_bool($data[$key]) ? true : throwError("Invalid boolean input.");
+							break;
+							case 'integer':
+								$check = is_int($data[$key]) ? true : throwError("Invalid integer input");
+							break;
+							case 'string':
+								$check = is_string($data[$key]) ? true : throwError("Invalid string input");
+							break;
+							case 'float':
+								$check = is_float($data[$key]) ? true : throwError("Invalid float input.");
+							break;
+							default:
+							throwError($type . " is not a valid type."); // type not found
+						}
 					}
 				}
+					
 			}
-				
 		}
-	}
 
-	//implement the conversion from json to xml
-	$xml = arrayToXml($data,"",false);
-	response(200, "Successfully completed process", $xml);
-}catch(\Exception $e){
-	// display exception/error message
-	response(500, $e->getMessage(),[]);
+		//implement the conversion from json to xml
+		$xml = arrayToXml($data,"",false);
+		response(200, "Successfully completed process", $xml);
+	}catch(\Exception $e){
+		// display exception/error message
+		response(500, $e->getMessage(),[]);
+	}
+}else{
+	if(!isset($data->from_msisdn))
+		response(405,'from_msisdn field is required',[]);
+	if(!isset($data->to_msisdn))
+		response(405,'to_msisdn field is required',[]);
+	if(!isset($data->message))
+		response(405,'message field is required',[]);
 }
 
+// json response
 function response($status,$status_message,$data)
 {
 	header("HTTP/1.1 ".$status);
--- test_cases.php
+++ test_cases.php
@@ -2,6 +2,10 @@
 
 require("vendor/autoload.php");
 
+assert_options(ASSERT_ACTIVE, 1);
+assert_options(ASSERT_WARNING, 0);
+assert_options(ASSERT_QUIET_EVAL, 1);
+
 function test($data)
 {
     $client = new \GuzzleHttp\Client([
