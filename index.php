85a146c8ae0ef4d1e2d25e157afa982b8b1da563
sadeeqilu <ilusadiq@gmail.com>
2021-06-17 13:33:00 +0100

    fixed errors, and exception handling

--- index.php
+++ index.php
@@ -24,102 +24,104 @@ try{
     // get post data
     $data = json_decode(file_get_contents('php://input'),true);
 }catch(\Exception $e){
-    response(500, $e->getMessage());
+    response(200, "Exception error.",[]);
+}
+
+// check if all required fields are available and 
+// display error message for any first missing required field
+if(!isset($data['from_msisdn'])){
+    response(200,'from_msisdn field is required',[]);
     return;
 }
 
-// check if all required fields are available
-if(!isset($data['from_msisdn']) || !isset($data['to_msisdn']) || !isset($data['message'])){
-
-    // display error message for any first missing required field
-    if(!isset($data['from_msisdn']))
-        response(200,'from_msisdn field is required',[]);
-    elseif(!isset($data['to_msisdn']))
-        response(200,'to_msisdn field is required',[]);
-        elseif(!isset($data['message']))
-        response(200,'message field is required',[]);
+if(!isset($data['to_msisdn'])){
+    response(200,'to_msisdn field is required',[]);
     return;
-    
 }
-try{
-    // validate input data types
-    if(!is_int($data['from_msisdn'])){
-        response(200,'Invalid integer input',[]);
-        return;
-    }
-    if(!is_string($data['message'])){
-        response(200,'Invalid string input',[]);
-        return;
-    }
-    if(!is_int($data['to_msisdn'])){
-        response(200,'Invalid integer input',[]);
+
+if(!isset($data['message'])){
+    response(200,'message field is required',[]);
+    return; 
+}
+
+// validate input data types
+if(!is_int($data['from_msisdn'])){
+    response(200,'Invalid integer input',[]);
+    return;
+}
+if(!is_string($data['message'])){
+    response(200,'Invalid string input',[]);
+    return;
+}
+if(!is_int($data['to_msisdn'])){
+    response(200,'Invalid integer input',[]);
+    return;
+}
+
+// if data has more than 4 inputs, that means extra fields are in the request as well
+if(count($data) > 4){
+    // check if field_map variable is available
+    if(!isset($data['field_map'])){
+        response(200,"field_map does not exist");
         return;
     }
 
-    // if data has more than 4 inputs, that means extra fields are in the request as well
-    if(count($data) > 4){
-        // check if field_map variable is available
-        if(!isset($data['field_map'])){
-            response(200,"field_map does not exist");
-            return;
-        }
+    // loop through all data to get extra fields
+    foreach($data as $key => $value){
+        // if key is one the required fields or the field_map object, move to the next key
+        if($key == "from_msisdn" || $key == "to_msisdn" || $key == "message" || $key == "field_map")
+            continue;
+        else {
+            // check if key is in field_map object
+            if(array_key_exists($key,$data['field_map'])){
+                // get the type of the field
+                $type = $data['field_map'][$key];
 
-        // loop through all data to get extra fields
-        foreach($data as $key => $value){
-            // if key is one the required fields or the field_map object, move to the next key
-            if($key == "from_msisdn" || $key == "to_msisdn" || $key == "message" || $key == "field_map")
-                continue;
-            else {
-                // check if key is in field_map object
-                if(array_key_exists($key,$data['field_map'])){
-                    // get the type of the field
-                    $type = $data['field_map'][$key];
-
-                    // validate the key type (share with users this naming convention)
-                    switch($type){
-                        case 'boolean':
-                            if(!is_bool($data[$key])){
-                                response(200,"Invalid boolean input.",[]);
-                                return;
-                            } 
-                        break;
-                        case 'integer':
-                            if(!is_int($data[$key])){
-                                response(200,"Invalid integer input");
-                                return;
-                            }
-                        break;
-                        case 'string':
-                            if(!is_string($data[$key])){
-                                response(200,"Invalid string input");
-                                return;
-                            }
-                        break;
-                        case 'float':
-                            if(!is_float($data[$key])){
-                                response(200,"Invalid float input.");
-                                return;
-                            }
-                        break;
-                        default:
-                        response(200,$type . " is not a valid type."); // type not found
-                        return;
-                    }
-                }else {
-                    response(200, $key . " does not exist in field_map");
+                // validate the key type (share with users this naming convention)
+                switch($type){
+                    case 'boolean':
+                        if(!is_bool($data[$key])){
+                            response(200,"Invalid boolean input.",[]);
+                            return;
+                        } 
+                    break;
+                    case 'integer':
+                        if(!is_int($data[$key])){
+                            response(200,"Invalid integer input");
+                            return;
+                        }
+                    break;
+                    case 'string':
+                        if(!is_string($data[$key])){
+                            response(200,"Invalid string input");
+                            return;
+                        }
+                    break;
+                    case 'float':
+                        if(!is_float($data[$key])){
+                            response(200,"Invalid float input.");
+                            return;
+                        }
+                    break;
+                    default:
+                    response(200,$type . " is not a valid type."); // type not found
                     return;
                 }
+            }else {
+                response(200, $key . " does not exist in field_map");
+                return;
             }
-                    
         }
+                
     }
-
+}
+try{
     //implement the conversion from json to xml
     $xml = arrayToXml($data,"",false);
     response(200, "Successfully completed process", $xml);
 }catch(\Exception $e){
     // display exception/error message
-    response(500, $e->getMessage(),[]);
+    response(200, "Exception error.",[]);
 }
 
 // json response
